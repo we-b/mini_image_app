85d6b1cb47635be0693d50aab659bad2dcab27e3
shouya0331 <USER_INFO@users.noreply.github.com>
2020-07-23 14:31:00 +0900

    TMに反映させるJS完成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,16 +1,43 @@
-document.addEventListener('DOMContentLoaded', function (){
-  const ImageList = document.getElementById('image-list')
+if (window.location.pathname === '/messages/new') {
+  document.addEventListener('load', function (){
+    const ImageList = document.getElementById('image-list')
 
-  const CreateImageHTML = (blob) => {
-    let img = document.createElement('img')
-    img.setAttribute('src', blob)
-    ImageList.appendChild(img)
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('id', "image-element")
+      let imageElementNum = document.querySelectorAll('#image-element')
 
-  }
+      // 表示する画像を生成
+      const img = document.createElement('img')
+      img.setAttribute('src', blob)
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `message_images_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
+
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(img)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+
     document.getElementById('message_images').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
 
-      CreateImageHTML(blob)
+      createImageHTML(blob)
+
     });
   });
+}
