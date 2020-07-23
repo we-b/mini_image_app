e4c97442f38b56b71780797d3ce460dc1ae1716c
shouya0331 <USER_INFO@users.noreply.github.com>
2020-07-23 12:16:11 +0900

    取得した画像情報のURLを生成しましょう。

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
   });
 });
