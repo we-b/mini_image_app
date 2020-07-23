3090c0f0fd605292b02b34f34dd18c3508f2cce3
shouya0331 <USER_INFO@users.noreply.github.com>
2020-07-23 12:34:46 +0900

    label要素をクリックしたら関数が動くようにしましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function (){
-  // ここから削除
-  // const imageContent = document.querySelector('img')
-  // if (imageContent){
-  // imageContent.remove();
-  // }
-  // ここまで削除
+  const clickLabel = () => {
+    const inputAll = document.getElementsByName('message[images][]');
+    const inputLast = inputAll[inputAll.length -1];
+    const inputId = inputLast.getAttribute('id');
+    console.log(inputId);
+    document.getElementById('click-upload').setAttribute('for', inputId);
+    return inputLast;
+  };
+
+  const changeInput = (inputArrayLast) => {
     document.getElementById('message_images').addEventListener('change', function(e){
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
@@ -13,4 +17,10 @@ document.addEventListener('DOMContentLoaded', function (){
       document.getElementById('new_message').insertAdjacentHTML('beforebegin', blobHTML);
       document.getElementById('new_message').insertAdjacentHTML('beforeend', inputHTML);
     });
+  };
+
+  document.getElementById('click-upload').addEventListener('click', function(){
+    const inputArrayLast = clickLabel();
+    changeInput(inputArrayLast);
   });
+});
