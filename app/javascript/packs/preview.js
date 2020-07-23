f75e2a07ba75a04d4e072235f50b86cb46b0fc9d
shouya0331 <USER_INFO@users.noreply.github.com>
2020-07-23 12:17:40 +0900

    生成したsrcを用いてプレビューを挿入しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
+    const blobHTML = `<img src="${blob}">`;
+    document.getElementById('new_message').insertAdjacentHTML('afterend' , blobHTML);
   });
 });
