7dbaf0335bce417ce47c8f6acbe165dff75ae72d
shouya0331 <USER_INFO@users.noreply.github.com>
2020-07-23 12:18:55 +0900

    表示されている画像を編集しましょう

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('message_image').addEventListener('change', function(e){
+    const imageContent = document.getElementById('image');
+    if (imageContent){
+      imageContent.remove();
+    };
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
     const blobHTML = `<img src="${blob}">`;
