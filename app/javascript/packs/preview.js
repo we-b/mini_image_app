016c4d05178c5a941d68033b5c3dd740afa6738f
yait-ucess <bandai28@icloud.com>
2021-01-19 13:34:16 +0900

    ２枚目のファイル選択ボタン
    ・input要素生成
    ・input要素に属性と属性値の付与
    ・input要素の表示

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -6,12 +6,22 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
      const createImageHTML = (blob) => {
         // 画像を表示するためのdiv要素を生成
        const imageElement = document.createElement('div')
+       imageElement.setAttribute('class', "image-element")
+       let imageElementNum = document.querySelectorAll('.image-element').length
  
        // 表示する画像を生成
        const blobImage = document.createElement('img')
        blobImage.setAttribute('src', blob)
+ 
+       // ファイル選択ボタンを生成
+       const inputHTML = document.createElement('input')
+       inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+       inputHTML.setAttribute('name', 'message[images][]')
+       inputHTML.setAttribute('type', 'file')
+ 
        // 生成したHTMLの要素をブラウザに表示させる
        imageElement.appendChild(blobImage)
+       imageElement.appendChild(inputHTML)
        ImageList.appendChild(imageElement)
      }
  
