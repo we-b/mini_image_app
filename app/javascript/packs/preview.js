c2a12bf9cedf33100d802ea981ef16282bdb367c
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2021-01-22 13:24:11 +0900

    Update preview.js

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+
+  document.getElementById('message_image').addEventListener('change', function(e){
+    console.log(e);
+  });
 });
