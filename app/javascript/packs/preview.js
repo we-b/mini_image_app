8a546927c2d366cfdd43ddc978cbbd3e652484de
WataruOjima <wataru.ojima20@gmail.com>
2021-02-01 12:48:19 +0900

    表示されている画像を編集可能
    編集ページでは、投稿に紐付いている画像があらかじめプレビュー表示されています。画像を編集するときは、既存のプレビューを削除した後に新しい画像のプレビューを表示するようにしましょう。if文を使用して、画像が表示されている場合には、そのimg要素をremoveします。

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,6 +2,16 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
     document.getElementById('message_image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
       const file = e.target.file[0];
       const blob = window.URL.createObjectURL(file);
 
@@ -10,7 +20,8 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
 
       // 表示する画像を生成
       const blobImage = document.createElement('img');
-      blobImage.setAttribute("src", blob);
+      blobImage.setAttribute('src', blob);
+      
 
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
