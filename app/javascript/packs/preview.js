782cdb154aad7ad10e82fd80bbb627444fe98fce
kamikazenohiro <kamikazenohiro632@icloud.com>
2021-02-05 11:41:03 +0900

    プレビュー機能で一枚だけ表示で機能完成

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,11 +2,8 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
     const ImageList = document.getElementById('image-list');
 
-    document.getElementById('message_image').addEventListener('change', function(e){
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-
-      // 画像を表示するためのdiv要素を生成
+    const createImageHTML = (blob) => {
+       // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
 
       // 表示する画像を生成
@@ -16,6 +13,19 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('message_image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
     });
   });
 }
