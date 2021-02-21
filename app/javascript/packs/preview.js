6ff4c9832687fea8a6c931e72e6efa87283d68bc
kenjitsumiyama <kenjitsumiyama0724@gmail.com>
2021-02-21 18:56:16 +0900

    Update preview.js

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -4,4 +4,4 @@ document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
   });
-});
+}); 
