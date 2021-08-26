4631aa0a71d0f8b6dae6eebfdcb0c37afa35472e
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-08-27 03:14:58 +0900

    2nd commit

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,25 +1,43 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
   if ( document.getElementById('message_image')){
-    const ImageList = document.getElementById('image-list');
+    const ImageList = document.getElementById('image-list')
 
+    // 選択した画像を表示する関数
     const createImageHTML = (blob) => {
-      const imageElement = document.createElement('div');                             //createElement('要素')メソッドで要素作ってる
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);                                            //画像情報(blob)を生成したimg要素のsrc属性に指定する
-      imageElement.appendChild(blobImage);                                            //作った要素をブラウザに表示 親要素.appendChild(追加する子要素);
-      ImageList.appendChild(imageElement);                                            //ImageList > imageElement > blobImage 
-    };
+      // 表示する画像を生成
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
 
-    document.getElementById('message_image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');                             //画像が表示されている場合のみ、既に表示されている画像を削除する記述
-      if (imageContent){
-        imageContent.remove();
-      }
-      const file = e.target.files[0];                                                 //イベントハッシュeのtargetの中からfileを取得、代入
-      const blob = window.URL.createObjectURL(file);                                  //取得した画像のURL作ってる
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
 
-      createImageHTML(blob);
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById('message_image').addEventListener('change', (e) => {
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+
     });
   }
 });
