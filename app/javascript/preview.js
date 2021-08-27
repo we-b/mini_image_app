774fb116fe979212ab2f38e1de53f18625f9d0e5
hirosetomoaki <tomokyun@live.jp>
2021-08-28 08:59:53 +0900

    プレビュー機能完成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,8 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
+  if ( document.getElementById('message_image')){
+    const ImageList = document.getElementById('image-list');
 
-  document.getElementById('message_image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-  });
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+       // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
+
+      // 表示する画像を生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('message_image').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0]; //関数の引数から選択した画像の情報を取得できる
+      const blob = window.URL.createObjectURL(file); //引数に指定したオブジェクトのURLを生成できる
+
+      createImageHTML(blob);
+    });
+  }
 });
